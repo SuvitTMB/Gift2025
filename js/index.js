8d6290519d79e8fd2bc37ed9dc4bc982ce9c3e28
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2024-08-04 11:14:19 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -57,6 +57,8 @@ async function getUserProfile() {
   str += '<div><img src="'+ sessionStorage.getItem("LinePicture") +'" class="add-profile" width="100px"></div>';
   str += '<div class="NameLine">'+ sessionStorage.getItem("LineName")+'</div>';
   $("#MyProfile").html(str);  
+  Connect_DB();
+  CheckData();
   //Connect_DB();
 }
 
